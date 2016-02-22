9bf61436bd03abc906ae65de13cb103e9b0eebfd
imashkan <ashkanpj.81@gmail.com>
2016-02-22 20:08:12 +0330

    updated haul bot

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,29 +226,18 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {81444630},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[Telehaul v2 - Open Source
+An advance Administration bot based ashi
 
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@ashi_king_kaboos {FOUNDER}
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+ASHI
 
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+.
 ]],
     help_text_realm = [[
 Realm Commands:
